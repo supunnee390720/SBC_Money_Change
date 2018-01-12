225ac3c0993f5648b90f240cb3a2a80550328f8d
supunnee <pum390720@gmail.com>
2018-01-12 16:07:33 +0700

    Test

--- app/src/main/java/supunnee/sbc/ac/th/sbcmoneychange/MainActivity.java
+++ app/src/main/java/supunnee/sbc/ac/th/sbcmoneychange/MainActivity.java
@@ -3,11 +3,22 @@ package supunnee.sbc.ac.th.sbcmoneychange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import supunnee.sbc.ac.th.sbcmoneychange.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+
+    }//Main Method
+}//main Class
